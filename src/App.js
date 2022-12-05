5194d22bc1f7a1d2f2294503b93a42142abb731d
yudhawan <yudhaashter@gmail.com>
2022-12-05 08:27:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 function App() {
+  const [tgl,setTgl] = useState(new Date())
+  const events = ['01-08-2022','07-08-2022','11-08-2022','20-08-2022','15-08-2022','29-08-2022']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full p-10">
+      <Calendar className="w-96 h-full rounded-xl bg-violet-300" onChange={setTgl} value={tgl}
+        tileClassName={({date})=>{
+          let day = date.getDate()
+          let month = date.getMonth()+1
+          if(date.getMonth()<10){
+            month = '0'+month
+          }
+          if(date.getDate()<10){
+            day = '0'+day
+          }
+          const realDate = day+'-'+month+'-'+date.getFullYear()
+          if(events.find(val=> val===realDate)){
+            return 'highlight'
+          }
+        }}
+       />
     </div>
   );
 }
